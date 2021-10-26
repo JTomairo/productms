f880c7c09b693216a02c294e76e62ace534edf0d
Juan Tomairo <jtomairo@everis.com>
2021-10-26 13:21:58 -0500

    fix: id Product

--- src/main/java/com/everis/craig/productms/documents/Product.java
+++ src/main/java/com/everis/craig/productms/documents/Product.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Product {
 
     @Id
-    private String Id;
+    private String id;
     private String productName;
     private String productYpe;
     private Politic politic;
